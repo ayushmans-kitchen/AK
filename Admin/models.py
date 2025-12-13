202ae5aecd9601580b704a1f03d7e93fe7d752b9
Anshuman Rath <94098687+rath-anshuman@users.noreply.github.com>
2025-12-14 02:59:51 +0530

    User Dashboard and Profile completed

--- Admin/models.py
+++ Admin/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class AdminNotice(models.Model):
+    message=models.CharField(max_length=500)
+    date=models.DateField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.message} - {self.date}"
+    
+    
--- Customers/urls.py
+++ Customers/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path 
-from Customers.views import user_dashboard, user_history,user_lunch_form,user_dinner_form
+from Customers.views import user_dashboard,user_profile, user_history,user_lunch_form,user_dinner_form
 
-from .loginview import login_view
+from .loginview import login_view,logout_view
 
 urlpatterns =[
     path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+
     path('', user_dashboard,name="user_dashboard"),
+    path('user-profile', user_profile,name="user_profile"),
+    path('user-history', user_history,name="user_history"),
+    
     path('userlunch/', user_lunch_form,name="userlunch"),
     path('userdinner/', user_dinner_form,name="userdinner"),
-    path('user-history', user_history,name="user_history"),
 ]
