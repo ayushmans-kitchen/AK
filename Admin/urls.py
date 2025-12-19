a676e36a11ce10b82d39a3d5e7aabf40fcded964
Vivek Mahakur <vivekmahakur2002@gmail.com>
2025-12-20 01:18:04 +0530

    usless data delete

--- Admin/urls.py
+++ Admin/urls.py
@@ -1,15 +1,9 @@
 from django.urls import path, include
-from Admin.views import admin_dashboard, service_list, daily_report, complete_report, dashboard, subscribers, profile
+from Admin.views import dashboard, service_details, subscribers, profile
 
 urlpatterns = [
-    path('admin-dashboard/',admin_dashboard,name='admin_dashboard'),
-    path("service/<str:meal>/<str:service>/", service_list, name="service_list"),
-    path("daily-report/", daily_report, name="daily_report"),
-    path("complete-report/", complete_report, name="complete_report"),
-
-
-
     path('dashboard/',dashboard,name='ayushman_dashboard'),
+    path('service-details/',service_details,name='service_details'),
     path('subscribers/',subscribers,name='subscribers'),
     path('profile/',profile,name='profile'),
 
